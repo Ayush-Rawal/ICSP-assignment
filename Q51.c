31ef040f609638a213e3bb6430912a14c99e9739
Ayush Rawal <31813648+Oguz-Khagan@users.noreply.github.com>
2017-11-23 18:05:57 +0530

    added questions 1 and 2

---
+++ Q51.c
@@ -0,0 +1,20 @@
+//Write a program to read and display n numbers using an array.
+#include<stdio.h>
+
+int main(void)
+{
+    int size,i;
+    printf("Enter size of array");
+    scanf("%d",&size);
+	printf("\n");
+    int arr[size];
+    for(i=0;i<n;i++){
+        scanf("%d",arr+i);
+    }
+	for(i=0;i<n;i++){
+	    printf("You entered:\n");
+		printf(" %d",arr[i])
+	}
+    return 0;
+
+}
---
+++ Q52.c
@@ -0,0 +1,17 @@
+//Write a program to print the position of the smallest of n numbers using an array 
+#include<stdio.h>
+int main(void)
+{
+    int size,i,small,pos;
+    printf("Enter array size ");
+    scanf("%d",&size);
+    int arr[size];
+    printf("\nEnter array\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i])
+		small=a[pos];
+		pos=(a[i]<small)?i:a[0];
+    }
+	printf"\nSmallest element is: %d and position(index) is %d",a[pos],pos);
+	return 0;
+}
